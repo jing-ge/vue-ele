5d95a943792587aac750fd5dd88be7f171113e79
jing-ge <675324370@qq.com>
2017-12-20 22:03:36 +0800

    finish goods css

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -14,9 +14,9 @@
 				<li v-for="item in goods" class="food-list">
 					<h1 class="food-title">{{item.name}}</h1>
 					<ul>
-						<li v-for="food in item.foods" class="food-item">
+						<li v-for="food in item.foods" class="food-item border-1px">
 							<div class="food-icon">
-								<img :src="food.icon">
+								<img width="57" height="57" :src="food.icon">
 							</div>
 							<div class="food-content">
 								<h2 class="food-name">{{food.name}}</h2>
@@ -26,8 +26,8 @@
 									<span>好评率{{food.rating}}%</span>
 								</div>
 								<div class="price">
-									<span>￥{{food.price}}</span>
-									<span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+									<span class="price-now">￥{{food.price}}</span>
+									<span class="price-old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
 								</div>
 							</div>
 						</li>
@@ -126,5 +126,64 @@ export default {
 	}
 	.guraantee{
 		background-image: url("guarantee_3@2x.png");
+	}
+	.food-title{
+		padding-left: 14px;
+		height: 26px;
+		line-height: 26px;
+		border-left: 2px solid #d9dde1;
+		font-size: 12px;
+		color: rgb(147,153,159);
+		background: #f3f5f7;
+	}
+	.food-item{
+		display: flex;
+		margin: 18px;
+		padding-bottom: 18px;
+	    border-top: 1px solid rgba(1,17,27,0.1);
+	}
+	.food-item:last-child{
+		display: none;
+		margin-bottom: 0px;
+	}
+	.food-icon{
+		flex: 0 0 57px;
+		margin-right: 10px;
+	}
+	.food-content{
+		flex:1;
+	}
+	.food-name{
+		font-size: 14px;
+		margin: 2px 0 8px 0;
+		height: 14px;
+		line-height: 14px;
+		color: rgb(7,17,27);
+	}
+	.food-desc, .food-extra{
+		font-size: 10px;
+		line-height: 10px;
+		color: rgb(147,153,159);
+	}
+	.food-desc{
+		margin-bottom: 8px;
+	}
+	.food-extra{
+		line-height: 10px;
+		margin-right: 12px;
+	}
+	.price{
+		font-weight: 700;
+		line-height: 24px;
+	}
+	.price-now{
+		margin-right: 8px;
+		font-size: 14px;
+		color: rgb(240,20,20)
+	}
+	.price-old{
+		text-decoration: line-through;
+		font-size: 10px;
+		color: rgb(147,153,159);
 	}
 </style>
